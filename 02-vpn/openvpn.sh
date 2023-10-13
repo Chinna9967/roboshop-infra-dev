3e785304005ae744e138dc74e41919bad71fd3fc
kashireddy <kashireddynaani9967@gmail.com>
2023-10-13 17:11:29 +0530

    r d files

--- 02-vpn/openvpn.sh
+++ 02-vpn/openvpn.sh
@@ -9,7 +9,7 @@ export PROTOCOL_CHOICE=2
 export DNS=1
 export COMPRESSION_ENABLED=n
 export CUSTOMIZE_ENC=n
-export CLIENT=kashi
+export CLIENT=kahsi
 export PASS=1
 curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
 chmod +x openvpn-install.sh
